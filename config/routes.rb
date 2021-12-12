4d7950cb325180f29c3b725d513d18dde7582224
Santosh Kumar Patro <santoshkumarpatro23@gmail.com>
2021-12-12 21:14:51 +0530

    basic sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # GET /about
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   # get "/", to: "main#index"
   root to: "main#index"
 end
